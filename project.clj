0f66ad8383057773c1744ea61afa9aa00e13739b
Jeff Weiss <jweiss@redhat.com>
2012-05-10 17:10:08 -0400

    update project file

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.redhat.qe/bz-checker "1.0.2"
-  :description "A library of automated testing related functions."
+  :description "a small library to check the status of a bugzilla bug."
   :java-source-path "src"
   :dependencies [[com.redhat.qe/xmlrpc-client-tools "1.0.3"]]
   :javac-options {:debug "on"})
