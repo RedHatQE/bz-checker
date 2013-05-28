2eefb7162a884a365157da6d35d0a9de30e6de5b
James Molet <jmolet@redhat.com>
2013-05-28 09:04:05 -0400

    upgrading to lein2

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject com.redhat.qe/bz-checker "1.0.3-SNAPSHOT"
   :description "a small library to check the status of a bugzilla bug."
-  :java-source-path "src"
+  :java-source-path "src" ;; lein1
+  :java-source-paths ["src"]
   :dependencies [[com.redhat.qe/xmlrpc-client-tools "1.0.3"]]
+  :plugins [[lein2-eclipse "2.0.0"]]
   :javac-options {:debug "on"})
