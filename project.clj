9ebb766af128ff6e832f46eb62e0ae273198465a
Filip Brychta <fbrychta@redhat.com>
2016-03-09 13:13:37 +0100

    Bump version of xmlrpc-client-tools

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "a small library to check the status of a bugzilla bug."
   :java-source-path "src" ;; lein1
   :java-source-paths ["src"]
-  :dependencies [[com.redhat.qe/xmlrpc-client-tools "1.0.3"]]
+  :dependencies [[com.redhat.qe/xmlrpc-client-tools "1.0.4"]]
   :plugins [[lein2-eclipse "2.0.0"]]
   :javac-options {:debug "on"})
