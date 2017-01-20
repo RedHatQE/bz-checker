d82ea64b671ef0af700b0a62854435768e464565
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-01-20 17:07:51 +0100

    Guice module for using XMLRPC implementation of BugzillaAPI. With a small example of usage.

---
+++ src/com/redhat/qe/auto/bugzilla/XMLRPCModule.java
@@ -0,0 +1,25 @@
+package com.redhat.qe.auto.bugzilla;
+
+import java.lang.Exception;
+
+/* Guice module for using XMLRPC_API as API for BzChecker */
+import com.redhat.qe.auto.bugzilla.BzChecker;
+import com.redhat.qe.auto.bugzilla.XMLRPC_API;
+import com.redhat.qe.auto.bugzilla.BugzillaAPI;
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class XMLRPCModule extends AbstractModule {
+    @Override
+    protected void configure(){
+        /* We can use XMLRPC_API class in every place where
+           BugzillaAPI implementation is needed. */
+        bind(BugzillaAPI.class).to(XMLRPC_API.class);
+    }
+    public void  main() throws Exception {
+        Injector injector = Guice.createInjector(new XMLRPCModule());
+        BzChecker checker = injector.getInstance(BzChecker.class);
+        System.out.println(checker.getBugState("1"));
+    }
+}
