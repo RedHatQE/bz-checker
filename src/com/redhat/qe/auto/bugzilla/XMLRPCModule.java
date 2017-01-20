2122f544c615d5b936bb301f8f990f0dc7eac8fb
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-01-20 20:56:08 +0100

    scope singleton for BzChecker

--- src/com/redhat/qe/auto/bugzilla/XMLRPCModule.java
+++ src/com/redhat/qe/auto/bugzilla/XMLRPCModule.java
@@ -9,13 +9,14 @@ import com.redhat.qe.auto.bugzilla.BugzillaAPI;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Singleton;
 
 public class XMLRPCModule extends AbstractModule {
     @Override
     protected void configure(){
         /* We can use XMLRPC_API class in every place where
            BugzillaAPI implementation is needed. */
-        bind(BugzillaAPI.class).to(XMLRPC_API.class);
+        bind(BugzillaAPI.class).to(XMLRPC_API.class).in(Singleton.class);
     }
     public void  main() throws Exception {
         Injector injector = Guice.createInjector(new XMLRPCModule());
