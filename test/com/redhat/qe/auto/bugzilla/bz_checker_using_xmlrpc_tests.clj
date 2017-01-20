8622ed7c0087363ec8c12c201e0ba39034e1f2e6
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-01-20 20:57:49 +0100

    aux tests for xmlrpc implementation of Bugzilla API.

--- test/com/redhat/qe/auto/bugzilla/bz_checker_using_xmlrpc_tests.clj
+++ test/com/redhat/qe/auto/bugzilla/bz_checker_using_xmlrpc_tests.clj
@@ -3,14 +3,14 @@
             [clojure.java.io :as io]
             [clojurewerkz.propertied.properties :as p]
             [clojure.java.io :as io]
-            )
+            [spyscope.core :refer :all]
+            [clojure.reflect :as r])
   (:import [com.redhat.qe.auto.bugzilla
             BzChecker
             BugzillaAPI
             BugzillaAPI$bzState
             XMLRPCModule]
-           [com.google.inject Guice]
-           ))
+           [com.google.inject Guice]))
 
 (def injector (atom nil))
 
@@ -19,16 +19,14 @@
                                                              "automation.properties.bugzilla.xmlrpc.testing"))]
                         (doseq [[k v] properties]
                           (System/setProperty k v)))
-                      ;;(reset! injector (Guice/createInjector (XMLRPCModule.)))
+                      (reset! injector (Guice/createInjector [(new XMLRPCModule)]))
                       (f)))
 
-
-;; (deftest bz-checker-main-get-methods-test
-;;   (let [checker (BzChecker/getInstance)]
-;;     (is (= BugzillaAPI$bzState/CLOSED (.getBugState checker "1")))
-;;     (is (= "CLOSED" (.getBugField checker "1" "status")))
-;;     (is (= "Bugzilla" (.getBugField checker "1" "product") ))
-;;     (is (= "bugzilla@redhat.com" (.getBugField checker "1" "qa_contact")))
-;;     (is (= #{"Reopened" "TestCaseApproved" "TestCaseRejected"}
-;;            (into #{} (.getBugField checker "1" "keywords"))))))
-
+(deftest bz-checker-main-get-methods-test
+  (let [checker (.getInstance @injector BzChecker)]
+    (is (= BugzillaAPI$bzState/CLOSED (.getBugState checker "1")))
+    (is (= "CLOSED" (.getBugField checker "1" "status")))
+    (is (= "Bugzilla" (.getBugField checker "1" "product") ))
+    (is (= "bugzilla@redhat.com" (.getBugField checker "1" "qa_contact")))
+    (is (= #{"Reopened" "TestCaseApproved" "TestCaseRejected"}
+           (into #{} (.getBugField checker "1" "keywords"))))))
