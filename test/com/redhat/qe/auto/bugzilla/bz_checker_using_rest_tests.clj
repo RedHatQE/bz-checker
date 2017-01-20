3bdcb33fbafad8fbccb7cf3e4189ced5dc97c2d7
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-01-20 21:53:54 +0100

    init version of unit tests for rest bugzilla implementation

--- test/com/redhat/qe/auto/bugzilla/bz_checker_using_rest_tests.clj
+++ test/com/redhat/qe/auto/bugzilla/bz_checker_using_rest_tests.clj
@@ -3,12 +3,17 @@
             [clojure.java.io :as io]
             [clojurewerkz.propertied.properties :as p]
             [clojure.java.io :as io])
-  (:import (com.redhat.qe.auto.bugzilla BzCheckerUsingREST)
-           (com.redhat.qe.auto.bugzilla.BzChecker)
-           ))
+  (:import [com.redhat.qe.auto.bugzilla
+            BzChecker
+            BugzillaAPI
+            BugzillaAPI$bzState
+            XMLRPCModule]
+           [com.google.inject Guice]))
+
+(def injector (atom nil))
 
 (use-fixtures :once (fn [f]
-                      (let [properties (p/load-from (io/file (System/getProperty "user.home") "automation.properties"))]
+                      (let [properties (p/load-from (io/file (System/getProperty "user.home") "automation.properties.bugzilla.rest.testing"))]
                         (doseq [[k v] properties]
                           (System/setProperty k v)))
                       (f)))
