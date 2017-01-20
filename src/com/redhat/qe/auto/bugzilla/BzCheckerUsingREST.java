059942b0126e891f0c5b030fbdd6b2d88c2a40ed
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-01-20 17:07:18 +0100

    initial commit for REST implementation of BugzillaAPI

---
+++ src/com/redhat/qe/auto/bugzilla/BzCheckerUsingREST.java
@@ -0,0 +1,16 @@
+package com.redhat.qe.auto.bugzilla;
+
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.CloseableHttpResponse;
+
+import com.redhat.qe.auto.bugzilla.BzChecker;
+
+public class BzCheckerUsingREST {
+
+    public BzCheckerUsingREST(){
+        System.out.println("ahoj");
+    };
+}
