2babbef3a103b6af16b635aad1ae0d73c8730dce
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-02-03 17:11:11 +0100

    google guice module for REST API to be used

---
+++ src/com/redhat/qe/auto/bugzilla/RESTModule.java
@@ -0,0 +1,29 @@
+package com.redhat.qe.auto.bugzilla;
+
+import java.lang.Exception;
+
+/* Guice module for using REST_API as Bugzilla API for BzChecker */
+import com.redhat.qe.auto.bugzilla.BzChecker;
+import com.redhat.qe.auto.bugzilla.REST_API;
+import com.redhat.qe.auto.bugzilla.BugzillaAPI;
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Singleton;
+
+public class RESTModule extends AbstractModule {
+
+  @Override
+  protected void configure(){
+    /* We can use REST_API class in every place where
+       BugzillaAPI implementation is needed. */
+    bind(BugzillaAPI.class).to(REST_API.class).in(Singleton.class);
+  }
+
+
+  public void  main() throws Exception {
+    Injector injector = Guice.createInjector(new RESTModule());
+    BzChecker checker = injector.getInstance(BzChecker.class);
+    System.out.println(checker.getBugState("1"));
+  }
+}
