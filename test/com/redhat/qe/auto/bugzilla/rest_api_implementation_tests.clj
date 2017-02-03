67ed2a0b09d80f5a85082fd688c0e31fc9cadc0e
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-02-03 17:10:01 +0100

    integration tests for REST_API

---
+++ test/com/redhat/qe/auto/bugzilla/rest_api_implementation_tests.clj
@@ -0,0 +1,35 @@
+(ns com.redhat.qe.auto.bugzilla.rest-api-implementation-tests
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [clojurewerkz.propertied.properties :as p]
+            [clojure.java.io :as io])
+  (:import [com.redhat.qe.auto.bugzilla
+            BzChecker
+            BugzillaAPI
+            REST_API
+            BugzillaAPI$bzState
+            RESTModule]
+           [com.google.inject Guice])
+  )
+
+(def injector (atom nil))
+
+(use-fixtures :once (fn [f]
+                      (let [properties (p/load-from (io/file (System/getProperty "user.home") "automation.properties.bugzilla.rest.testing"))]
+                        (doseq [[k v] properties]
+                          (System/setProperty k v)))
+                      (reset! injector (Guice/createInjector [(new RESTModule)]))
+                      (f)))
+
+(deftest credentials-test
+  (is (= "rhq-xmlrpc@redhat.com" (System/getProperty "bugzilla.login"))))
+
+(deftest get-bug-test
+  (let [api (new REST_API)]
+    (.connectBZ api)
+    (let [bug (.getBug api "1")
+          bug-without-comments (dissoc (into {} bug) "comments")]
+      (is (= {"status" "ASSIGNED"
+              "product" "Bugzilla"
+              "keywords" ["Reopened" "TestCaseApproved" "TestCaseRejected"]
+              "qa_contact" "bugzilla@redhat.com"} bug-without-comments)))))
