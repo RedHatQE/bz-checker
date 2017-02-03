9c3abe2a289ffe908ef144f6f979cf239ec7a373
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-02-03 17:06:48 +0100

    new version of org.json is used

--- build.boot
+++ build.boot
@@ -1,5 +1,6 @@
 (set-env! :dependencies '[[adzerk/boot-test "1.1.2" :scope "test"]
                           [com.redhat.qe/xmlrpc-client-tools "1.0.5"]
+                          [org.json/json "20160810"]
                           [org.apache.httpcomponents/httpclient "4.5.2"]
                           [clojurewerkz/propertied "1.2.0"]
                           [spyscope "0.1.5"]
--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject com.redhat.qe/bz-checker "1.0.3-SNAPSHOT"
+(defproject com.redhat.qe/bz-checker "1.1.0-SNAPSHOT"
   :description "a small library to check the status of a bugzilla bug."
   :java-source-path "src" ;; lein1
   :java-source-paths ["src"]
   :javac-options ["-g" "-target" "1.8" "-source" "1.8"]
   :dependencies [[com.redhat.qe/xmlrpc-client-tools "1.0.5"]
                  [com.google.inject/guice "4.1.0"]
+                 [org.json/json "20160810"]
                  [org.apache.httpcomponents/httpclient "4.5.2"]
                  [clojurewerkz/propertied "1.2.0"]
                  [org.clojure/clojure "1.8.0"]]
