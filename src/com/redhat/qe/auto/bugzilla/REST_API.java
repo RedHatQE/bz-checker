794db66e5e6194b6b7bd2a15e400393f01abce6d
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-02-09 16:33:01 +0100

    refactor to use new BugzillaAPIException

--- src/com/redhat/qe/auto/bugzilla/REST_API.java
+++ src/com/redhat/qe/auto/bugzilla/REST_API.java
@@ -1,20 +1,14 @@
 package com.redhat.qe.auto.bugzilla;
 
-import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Properties;
-import java.util.logging.LogManager;
 import java.util.logging.Logger;
-import java.lang.Exception;
 import java.net.URI;
-import java.util.stream.Stream;
+import java.net.URISyntaxException;
 import java.util.Iterator;
 
 import java.io.IOException;
-import java.security.GeneralSecurityException;
 
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -24,8 +18,9 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.CloseableHttpResponse;
+
 import com.redhat.qe.auto.bugzilla.IBugzillaAPI;
-import org.apache.http.util.EntityUtils;
+import com.redhat.qe.auto.bugzilla.BugzillaAPIException;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -34,7 +29,7 @@ import org.json.JSONTokener;
 public class REST_API implements IBugzillaAPI {
 	protected static Logger log = Logger.getLogger(REST_API.class.getName());
 
-  private Map<String,Map> buglist;
+  private Map<String,Map<String,Object> > buglist;
   private URI uri;
   private CloseableHttpClient httpclient;
 
@@ -46,81 +41,103 @@ public class REST_API implements IBugzillaAPI {
                                       "tags"};
 
 
-  public REST_API() throws Exception {
-    buglist = new HashMap<String,Map>();
+  public REST_API(){
+    buglist = new HashMap<String, Map<String,Object> >();
   }
 
- 
-  public void connectBZ() throws Exception {
-    uri = new URI(System.getProperty("bugzilla.url") + "/rest");
-    CredentialsProvider credsProvider = new BasicCredentialsProvider();
-    credsProvider.setCredentials(AuthScope.ANY,
-                                 new UsernamePasswordCredentials(System.getProperty("bugzilla.login"),
-                                                                 System.getProperty("bugzilla.password")));
-    httpclient = HttpClients.custom()
-      .setDefaultCredentialsProvider(credsProvider)
-      .build();
+	public void connectBZ() throws BugzillaAPIException {
+    try {
+      uri = new URI(System.getProperty("bugzilla.url") + "/rest");
+      CredentialsProvider credsProvider = new BasicCredentialsProvider();
+      credsProvider.setCredentials(AuthScope.ANY,
+                                   new UsernamePasswordCredentials(System.getProperty("bugzilla.login"),
+                                                                   System.getProperty("bugzilla.password")));
+      httpclient = HttpClients.custom()
+        .setDefaultCredentialsProvider(credsProvider)
+        .build();
+    } catch (URISyntaxException ex) {
+      throw new BugzillaAPIException(ex.toString());
+    }
   }
 
 
-  public int login(String userid, String password) throws Exception{
+  public int login(String userid, String password) throws BugzillaAPIException{
     return 0;
   }
 
-  protected Map<String,Object> parseComment(JSONObject commentObj) throws Exception {
-    return new HashMap<String,Object> () {{
-      Iterator<String> it = commentObj.keys();
-      while(it.hasNext()){
-        String key = it.next();
-        put(key, commentObj.get(key));
-      }
-    }};
+  protected Map<String,Object> parseComment(JSONObject commentObj) {
+		return new HashMap<String, Object>() {
+				private static final long serialVersionUID = -7353628974554585200L;
+      {
+        Iterator<String> it = commentObj.keys();
+        while(it.hasNext()){
+          String key = it.next();
+          put(key, commentObj.get(key));
+        }
+      }};
   }
 
 
-  public Map<String, Object> getBug(String bugId) throws Exception {
-    URI newURI = new URI(uri.toString() + "/bug/" + bugId
-                         + "?api_key=" + System.getProperty("bugzilla.apikey")
-                         + "&include_fields=" + String.join(",", bugFields));
-    if( System.getProperty("bugzilla.cache","false").equals("true")){
-      Map<String,Object> bug = buglist.get(bugId);
-      if(bug != null) {
-        log.finer("using cached bugzilla " + bugId);
-        return bug;
-      }
-    }
-    CloseableHttpResponse response = httpclient.execute(new HttpGet(newURI.normalize()));
+	public Map<String, Object> getBug(String bugId) throws BugzillaAPIException {
     try {
-      if(response.getStatusLine().getStatusCode() != 200){
-        throw new Exception("Wrong status code returned. The status line was: "
-                            + response.getStatusLine());
-      };
-      JSONTokener tokener = new JSONTokener(response.getEntity().getContent());
-      JSONObject root = new JSONObject(tokener);
-      JSONArray bugs = (JSONArray) root.get("bugs");
-      JSONObject bugObj = (JSONObject) bugs.get(0);
-      Map<String,Object> bug = new HashMap<String,Object>() {{
-          put("status",bugObj.getString("status"));
-          put("product",bugObj.getString("product"));
-          put("qa_contact", bugObj.getString("qa_contact"));
-          put("keywords", new ArrayList<String> () {{
-            JSONArray keywords = bugObj.getJSONArray("keywords");
-            Iterator<Object> it = keywords.iterator();
-            while(it.hasNext()){
-              add((String)it.next());
-            }
-          }});
-          put("comments",  new ArrayList<Object>() {{
-            Iterator<Object> it = bugObj.getJSONArray("comments").iterator();
-            while(it.hasNext()){
-              add(parseComment((JSONObject)it.next()));
-            }
-          }});
-        }};
-      buglist.put(bugId,bug);
-      return bug;
-    } finally {
-      response.close();
+      URI newURI = new URI(uri.toString() + "/bug/" + bugId
+                           + "?api_key=" + System.getProperty("bugzilla.apikey")
+                           + "&include_fields=" + String.join(",", bugFields));
+      if( System.getProperty("bugzilla.cache","false").equals("true")){
+        Map<String,Object> bug = buglist.get(bugId);
+        if(bug != null) {
+          log.finer("using cached bugzilla " + bugId);
+          return bug;
+        }
+      }
+      CloseableHttpResponse response = null;
+      try {
+        response = httpclient.execute(new HttpGet(newURI.normalize()));
+        if(response.getStatusLine().getStatusCode() != 200){
+          throw new BugzillaAPIException("Wrong status code returned. The status line was: "
+                                + response.getStatusLine());
+        };
+        JSONTokener tokener = new JSONTokener(response.getEntity().getContent());
+        JSONObject root = new JSONObject(tokener);
+        JSONArray bugs = (JSONArray) root.get("bugs");
+        JSONObject bugObj = (JSONObject) bugs.get(0);
+				Map<String, Object> bug = new HashMap<String, Object>() {
+            private static final long serialVersionUID = 1L;
+            {
+              put("status",bugObj.getString("status"));
+              put("product",bugObj.getString("product"));
+              put("qa_contact", bugObj.getString("qa_contact"));
+              put("keywords", new ArrayList<String>() {
+                  private static final long serialVersionUID = 1L;
+                  {
+                    JSONArray keywords = bugObj.getJSONArray("keywords");
+                    Iterator<Object> it = keywords.iterator();
+                    while(it.hasNext()){
+                      add((String)it.next());
+                    }
+                  }});
+						put("comments", new ArrayList<Object>() {
+							private static final long serialVersionUID = 1L;
+                {
+                  Iterator<Object> it = bugObj.getJSONArray("comments").iterator();
+                  while(it.hasNext()){
+                    add(parseComment((JSONObject)it.next()));
+                  }
+                }});
+            }};
+        buglist.put(bugId,bug);
+        return bug;
+      } catch (IOException ex) {
+        throw new BugzillaAPIException(ex.toString());
+      } finally {
+				try {
+					response.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+      }
+    } catch (URISyntaxException ex) {
+      throw new BugzillaAPIException(ex.toString());
     }
   }
 
@@ -129,38 +146,37 @@ public class REST_API implements IBugzillaAPI {
    * Returns a Map containing an Array of Maps.  Within the innermost Maps (which represent bugs), there's another
    * Map under the key "internals", which has a key "bug_status".  ugh.
    */
-  public Object[] getBugs(Map<String, Object> values) throws Exception
+  public Object[] getBugs(Map<String, Object> values) throws BugzillaAPIException
   {
-    throw new Exception("not implemented yet");
-    //return null;
+    throw new BugzillaAPIException("not implemented yet");
   }
 
 
-  public Object[] getBugs(String name, Object value) throws Exception {
-    throw new Exception("not implemented yet");
+  public Object[] getBugs(String name, Object value) throws BugzillaAPIException {
+    throw new BugzillaAPIException("not implemented yet");
     // Map<String, Object> map = new HashMap<String, Object>();
     // map.put(name, value);
     // return getBugs(map);
   }
 
 
-  public Map update_bug_status(String bug_id, IBugzillaAPI.bzState newState) throws Exception{
-    throw new Exception("not implemented yet");
+  public Map<String,Object> update_bug_status(String bug_id, IBugzillaAPI.bzState newState) throws BugzillaAPIException{
+    throw new BugzillaAPIException("not implemented yet");
     // Map<String,Object> updates = new HashMap<String,Object>();
     // updates.put("bug_status", newState.toString());
     // return update_bug(bug_id, updates);
   }
 
 
-  public Map update_bug(String bug_id, Map<String,Object> updates)throws Exception{
-    throw new Exception("not implemented yet");
+  public Map<String,Object> update_bug(String bug_id, Map<String,Object> updates)throws BugzillaAPIException{
+    throw new BugzillaAPIException("not implemented yet");
     // Map map = new HashMap<String,Object>();
     // return map;
   }
 
 
-  public Map add_bug_comment(String bug_id, String comment) throws Exception{
-    throw new Exception("not implemented yet");
+  public Map<String,Object> add_bug_comment(String bug_id, String comment) throws BugzillaAPIException{
+    throw new BugzillaAPIException("not implemented yet");
     // Map<String,Object> map = new HashMap<String,Object>();
     // return map;
   }
