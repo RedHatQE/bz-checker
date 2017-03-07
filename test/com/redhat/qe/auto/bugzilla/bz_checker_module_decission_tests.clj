62caa0c40499f4c200101837b90c15538e9b9f00
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-03-07 20:07:46 +0100

    aux test for the type of implementation decission.

---
+++ test/com/redhat/qe/auto/bugzilla/bz_checker_module_decission_tests.clj
@@ -0,0 +1,28 @@
+(ns com.redhat.qe.auto.bugzilla.bz-checker-module-decission-tests
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [clojurewerkz.propertied.properties :as p]
+            [clojure.java.io :as io]
+            [spyscope.core :refer :all]
+            [clojure.reflect :as r])
+  (:import [com.redhat.qe.auto.bugzilla
+            BzChecker
+            IBugzillaAPI$bzState
+            RESTModule XMLRPCModule])
+  )
+
+(use-fixtures :once (fn [f]
+                      (let [properties (p/load-from (io/file (System/getProperty "user.home")
+                                                             "automation.properties.bugzilla.rest.testing"))]
+                        (doseq [[k v] properties]
+                          (System/setProperty k v)))
+                      (f)))
+
+(deftest bz-checker-decides-implementation-module-test
+  (System/setProperty "bugzilla.url" "https://beta.bugzilla.redhat.com/bugzilla/rest")
+  (let [injection-module (BzChecker/getInjectionModule)]
+    (is (= "com.redhat.qe.auto.bugzilla.RESTModule" (.. injection-module getClass getName))))
+
+  (System/setProperty "bugzilla.url" "https://bugzilla.redhat.com/xmlrpc.cgi")
+  (let [injection-module (BzChecker/getInjectionModule)]
+    (is (= "com.redhat.qe.auto.bugzilla.XMLRPCModule" (.. injection-module getClass getName)))))
